d8de7852a56261820c0ece6ee21512a3408ea057
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-05 07:39:47 -0700

    got the HighestLowestDisplay container sketched out

---
+++ src/containers/highest-lowest-temp-display/HighestLowestDisplay.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import HighestLowest from '../../components/highest-lowest/HighestLowest';
+
+class HighestLowestDisplay extends Component {
+  componentDidMount() {
+    //a fetch might go here depending on how we handle the data
+  }
+
+  componentDidUpdate() {
+    //we might need this lifecycle depending on how we are using the data
+  }
+
+  render() {
+    return (
+      //we will need to give this component the avgTemp prop it requires
+      <HighestLowest />
+    );
+  }
+}
+
+// const mapStateToProps = state => ({
+//   for the highest and lowest location
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   if we are making a fetch it will live here
+// });
+
+export default connect(
+  // mapStateToProps,
+  // mapDispatchToProps
+)(HighestLowestDisplay);
+
+//TO DO
+//create the detail view page/container
+//make pages folder
+//add CORS to the backend
